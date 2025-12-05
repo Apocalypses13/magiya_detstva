656ceaa2d846a19aae8b26413cf6b40d42a6f652
Apocalypses13 <viktoriaklochk0vaa@gmail.com>
2025-12-05 09:51:28 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,43 +4,44 @@ const modal = document.getElementById('modal');
 const closeBtn = document.getElementById('closeBtn');
 const form = document.querySelector('.form');
 
-// Открыть окно заявки
-orderBtn.addEventListener('click', function() {
-    modal.style.display = 'flex';});
+// Когда жмем кнопку "Заказать"
+orderBtn.addEventListener('click', function() { 
+    modal.style.display = 'flex';}); // Показываем окно
 
-// Закрыть окно заявки
+// Способ 1 как закрыть окно: Клик на крестик
 closeBtn.addEventListener('click', function() {
-    modal.style.display = 'none';});
+    modal.style.display = 'none';}); // Прячем окно
 
-// Закрыть при клике вне окна
+// Способ 2: Клик на серый фон вокруг окна
 modal.addEventListener('click', function(e) {
-    if (e.target === modal) {
-        modal.style.display = 'none';}});
+    if (e.target === modal) { // Если кликнул именно на фон
+        modal.style.display = 'none';}}); // Прячем
 
-// Обработка формы
+// Отправка формы
 form.addEventListener('submit', function(e) {
-    e.preventDefault();
+    e.preventDefault(); // Не перезагружать страницу!
     
-    // Получаем значения из полей ввода
+    // Получаем значения из полей ввода,которые вписали
     const nameInput = this.querySelector('input[type="text"]');
     const phoneInput = this.querySelector('input[type="tel"]');
     
     const name = nameInput.value;
     const phone = phoneInput.value;
     
-    // Показываем персонализированное сообщение
+    // Показываем персонализированное уведомление
     alert(`Здравствуйте, ${name}! Мы вам перезвоним по номеру ${phone}, хорошего вам дня!`);
     
     // Закрываем окно и очищаем форму
-    modal.style.display = 'none';
-    this.reset();});
+    modal.style.display = 'none'; //Закрываем
+    this.reset();}); // Очищаем
 
-// Плавная прокрутка для якорных ссылок
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+// Плавная прокрутка для ссылок
+document.querySelectorAll('a[href^="#"]').forEach(anchor => { //Ищет все ссылки, которые ссылаются внутри этой же страницы и берет каждую по очереди
     anchor.addEventListener('click', function (e) {
-        e.preventDefault();
+        e.preventDefault(); //Отменяет обычное поведение ссылки (резкий прыжок).
         const target = document.querySelector(this.getAttribute('href'));
-        if (target) {
+        if (target) { //плавно открывает ссылки
             target.scrollIntoView({
                 behavior: 'smooth',
                 block: 'start' }); } });});
+
